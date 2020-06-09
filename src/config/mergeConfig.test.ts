4cf9b0ba4fa314271cef8f8b4d1560eae78372d7
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-06-09 13:31:27 +0200

    Add more tests

--- src/config/mergeConfig.test.ts
+++ src/config/mergeConfig.test.ts
@@ -8,4 +8,12 @@ describe("mergeConfig", () => {
     const mergedConfig = mergeConfig(config)
     expect(mergedConfig.api).toEqual({ additionalHeaders })
   })
+
+  it("undefined", () => {
+    const additionalHeaders = { Authorization: "Bearer token_abcdef" }
+    const config = { api: { additionalHeaders } }
+    const mergedConfig = mergeConfig(config)
+    const mergedConfig1 = mergeConfig(undefined, mergedConfig)
+    expect(mergedConfig.api).toEqual({ additionalHeaders })
+  })
 })
