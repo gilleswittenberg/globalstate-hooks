45e1d6b8ee08a3e09631fc38c1b0afa9fd4ff045
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-06-24 14:22:14 +0200

    Fix exporting immer

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import type { ItemsState, RecordState } from "./useRestReducer"
 import type { RequestHeaders, ResolvedRequest } from "./methods/fetch"
 import useRestActions, { ItemsActions } from "./useRestActions"
 import useRestRecordActions, { RecordActions } from "./useRestRecordActions"
+import immer from "immer"
 
 export type { ItemsState }
 export type { RecordState }
@@ -13,4 +14,4 @@ export type { RequestHeaders }
 export { useRestActions }
 export { useRestRecordActions }
 
-export * as immer from "immer"
+export { immer }
