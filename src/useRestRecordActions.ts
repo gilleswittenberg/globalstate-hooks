9adc632e68c278701fdd248cb03c1079ca1435dc
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-07-01 17:08:50 +0200

    Improve use of useCallback

--- src/useRestRecordActions.ts
+++ src/useRestRecordActions.ts
@@ -25,37 +25,37 @@ const useRestRecordActions = <Schema>(conf?: Partial<Config>, initialData?: Sche
 
   // index
   const indexConfig = mergeConfig(config.index, config)
-  const handleSuccessIndex = (result: Schema) => {
+  const handleSuccessIndex = useCallback((result: Schema) => {
     const { createSetData } = actionCreators
     dispatch(createSetData(result))
-  }
+  }, [ actionCreators, dispatch ])
   const index = useCreateAction<Schema, RecordAction<Schema>, RecordState<Schema>>(handleSuccessIndex as HandleSuccess<Schema>, shouldMakeRequest(indexConfig) ? "GET" : undefined, config, actionCreators, dispatch)
-  const indexStable = useCallback(index, [])
+  const indexStable = useCallback(index, [handleSuccessIndex])
 
   // update
   const updateConfig = mergeConfig(config.update, config)
-  const handleSuccessUpdate = (result: Schema) => {
+  const handleSuccessUpdate = useCallback((result: Schema) => {
     const { createSetData } = actionCreators
     dispatch(createSetData(result))
-  }
+  }, [ actionCreators, dispatch ])
   const update = useCreateAction<Schema, RecordAction<Schema>, RecordState<Schema>>(handleSuccessUpdate as HandleSuccess<Schema>, shouldMakeRequest(updateConfig) ? "POST" : undefined, config, actionCreators, dispatch)
-  const updateStable = useCallback(update, [])
+  const updateStable = useCallback(update, [handleSuccessUpdate])
 
   // clear
-  const handleSuccessClear = () => {
+  const handleSuccessClear = useCallback(() => {
     const { createClear } = actionCreators
     dispatch(createClear())
-  }
+  }, [ actionCreators, dispatch ])
   const clear = useCreateAction<Schema, RecordAction<Schema>, RecordState<Schema>>(handleSuccessClear as HandleSuccess<Schema>, undefined, config, actionCreators, dispatch)
-  const clearStable = useCallback(clear, [])
+  const clearStable = useCallback(clear, [handleSuccessClear])
 
   // update state (locally)
-  const handleSuccessUpdateState = (result: Schema) => {
+  const handleSuccessUpdateState = useCallback((result: Schema) => {
     const { createSetData } = actionCreators
     dispatch(createSetData(result))
-  }
+  }, [ actionCreators, dispatch ])
   const updateState = useCreateAction<Schema, RecordAction<Schema>, RecordState<Schema>>(handleSuccessUpdateState as HandleSuccess<Schema>, undefined, config, actionCreators, dispatch)
-  const updateStateStable = useCallback(updateState, [])
+  const updateStateStable = useCallback(updateState, [handleSuccessUpdateState])
 
   useEffect(() => {
     if (config.shouldIndex === false) return
